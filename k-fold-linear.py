b237ff0374ae538ef86f7c781650837676b04212
AssazziN <niszazsa@msn.com>
2018-05-07 22:11:19 +0700

    fix bug

--- k-fold-linear.py
+++ k-fold-linear.py
@@ -1,12 +1,13 @@
-import pandas
-from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
 from sklearn import metrics
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
 
 
 def import_data(stockName):
 	# import total dataset
-	data = pandas.read_csv('StocksData/' + stockName + '.csv')
+	data = pd.read_csv('StocksData/' + stockName + '.csv')
 	data = data.drop(['DATE'], 1)
 	data = data.dropna()
 
@@ -15,7 +16,10 @@ def import_data(stockName):
 
 	# separate into independent and dependent variables
 	x = data[headers[:-1]]
-	y = data[headers[-1:]].values.ravel()
+	y = data[headers[-1:]].values
+	y[y == "UP"] = 1
+	y[y == "DRAW"] = 0
+	y[y == "DOWN"] = -1
 
 	return x, y
 
@@ -25,20 +29,21 @@ if __name__ == '__main__':
 	x, y = import_data(stockName)
 
 	# set to 10 folds
-	skf = StratifiedKFold(n_splits=10)
+	skf = KFold(n_splits=10)
 
 	# blank lists to store predicted values and actual values
 	predicted_y = []
 	expected_y = []
 
 	# partition data
+	accuracy = 0
 	for train_index, test_index in skf.split(x, y):
 		# specific ".loc" syntax for working with dataframes
 		x_train, x_test = x.loc[train_index], x.loc[test_index]
 		y_train, y_test = y[train_index], y[test_index]
 
 		# create and fit classifier
-		classifier = GaussianNB()
+		classifier = LinearRegression()
 		classifier.fit(x_train, y_train)
 
 		# store result from classification
@@ -47,6 +52,9 @@ if __name__ == '__main__':
 		# store expected result for this specific fold
 		expected_y.extend(y_test)
 
-	# save and print accuracy
-	accuracy = metrics.accuracy_score(expected_y, predicted_y)
-print("GaussianNB accuracy with " + stockName + ": " + accuracy.__str__())
+		# save and print accuracy
+		accuracy += np.sqrt(metrics.mean_squared_error(expected_y, predicted_y))
+
+
+	#accuracy = metrics.accuracy_score(expected_y, predicted_y)
+print "Linear-regression accuracy with " + stockName + ": " , accuracy/10
