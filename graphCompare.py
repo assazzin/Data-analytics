976ba274916795b7df5a7c99b11b896947e213ce
AssazziN <niszazsa@msn.com>
2018-05-07 20:59:56 +0700

    add X label

--- graphCompare.py
+++ graphCompare.py
@@ -6,8 +6,8 @@ df = pd.read_csv('data.csv')
 df=df.drop(['DATE'], 1)
 
 # Plot 2 graps for correlation comparing
-stock1 = 'CPF'
-stock2 = 'TISCO'
+stock1 = 'WHA'
+stock2 = 'TCAP'
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
@@ -18,4 +18,5 @@ ax2 = ax1.twinx()
 ax2.plot(df[stock2], 'coral')
 ax2.set_ylabel(stock2, color='coral')
 
+ax1.set_xlabel('Days')
 plt.show()
